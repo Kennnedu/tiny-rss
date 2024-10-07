eb66bf3a5eeb33c0d03ae71c46fae66fa36fafa4
Kennnedu <r.kenda94@gmail.com>
2024-10-07 13:44:03 +0300

    refactored feeds spec

--- spec/integration/feeds_spec.rb
+++ spec/integration/feeds_spec.rb
@@ -2,24 +2,84 @@ require_relative '../spec_helper'
 
 describe 'Feeds' do
   before do
-    feeds = JSON.parse(File.read('/app/spec/test_files/feeds.json'))
-    posts = JSON.parse(File.read('/app/spec/test_files/posts.json'))
-    feeds.each { |feed| Feeds.insert(feed) }
-    posts.each { |post| Posts.insert(post) }
-    Posts.insert(feed_id: 2, link: "https:\/\/habr.com\/ru\/post\/695800\/?utm_source=habrahabr&utm_medium=rss&utm_campaign=695800", published_at: Time.now)
+    Feeds.insert(url: 'https://habr.com/ru/rss/all/all/?fl=ru')
   end
 
-  it 'index' do
+  let(:feed) { Feeds.all[0] }
+
+  it 'displays links' do
     visit '/'
     assert_link 'Today'
-    assert_text 'Today 1'
     assert_link 'Starred'
-    assert_text 'Starred 2'
     assert_link 'Read Later'
-    assert_text 'Read Later 1'
-    assert_link 'people.onliner.by'
-    assert_text 'people.onliner.by 1'
     assert_link 'habr.com'
-    assert_text 'habr.com 1'
+  end
+
+  describe 'starred post' do
+    before do
+      Posts.insert(
+        feed_id: feed[:id],
+        link: 'https://example.com',
+        published_at: '2022-08-23T10:49:56.000Z',
+        starred_at: '2022-08-24T05:55:13.428Z',
+      )
+    end
+
+    it 'increases starred and feed counters' do
+      visit '/'
+      assert_text 'Starred 1'
+      assert_text 'habr.com 1'
+    end
+    
+    it 'doesn\'t increase today and read_later counter' do
+      visit '/'
+      assert_no_text 'Read Later 1'
+      assert_no_text 'Today 1'
+    end
+  end
+
+  describe 'post marked as read_later' do
+    before do
+      Posts.insert(
+        feed_id: feed[:id],
+        link: 'https://example.com',
+        published_at: '2022-08-23T10:49:56.000Z',
+        read_later_at: '2022-08-24T05:55:13.428Z',
+      )
+    end
+
+    it 'increases read_later and feed counters' do
+      visit '/'
+      assert_text 'Read Later 1'
+      assert_text 'habr.com 1'
+    end
+
+    it 'doesn\'t increase starred and today counter' do
+      visit '/'
+      assert_no_text 'Starred 1'
+      assert_no_text 'Today 1'
+    end
+  end
+
+  describe 'post published today' do
+    before do
+      Posts.insert(
+        feed_id: feed[:id],
+        link: 'https://example.com',
+        published_at: Time.now.to_s,
+      )
+    end
+
+    it 'increases today and feed counters' do
+      visit '/'
+      assert_text 'Today 1'
+      assert_text 'habr.com 1'
+    end
+
+    it 'doesn\'t increase read_later and starred counters' do
+      visit '/'
+      assert_no_text 'Read Later 1'
+      assert_no_text 'Starred 1'
+    end
   end
 end
